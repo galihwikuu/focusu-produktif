fd7d535035e729ec14397f242c6290574d053d92
Galih Wiku <l200240164@student.ums.ac.id>
2026-05-10 20:35:40 +0700

    fix login session

--- server.js
+++ server.js
@@ -10,33 +10,34 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: '*'
+  origin: true,
+  credentials: true
 }));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(session({
-//   secret: 'taskapp-secret-2024',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//     secure: false,
-//     httpOnly: true,
-//     sameSite: 'lax',
-//     maxAge: 7 * 24 * 60 * 60 * 1000
-//   }
-// }));
+app.use(session({
+  secret: 'taskapp-secret-2024',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    sameSite: 'lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  }
+}));
 
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/tasks', require('./routes/tasks'));
-// app.use('/api/todos', require('./routes/todos'));
-// app.use('/api/schedules', require('./routes/schedules'));
-// app.use('/api/deadlines', require('./routes/deadlines'));
-// app.use('/api/finance', require('./routes/finance'));
-// app.use('/api/notes', require('./routes/notes'));
-// app.use('/api/admin', require('./routes/admin'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
+app.use('/api/todos', require('./routes/todos'));
+app.use('/api/schedules', require('./routes/schedules'));
+app.use('/api/deadlines', require('./routes/deadlines'));
+app.use('/api/finance', require('./routes/finance'));
+app.use('/api/notes', require('./routes/notes'));
+app.use('/api/admin', require('./routes/admin'));
 
 app.get('/', (req, res) => {
   res.send('Railway berhasil 🚀');
